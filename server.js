527721b8346fa81e602702f81f801ad1c97e47d9
Márton Borlay <Marci@Martons-MacBook-Pro.local>
2014-04-25 16:06:38 +0200

    Error messages now send proper error codes, and inserted nested collections assign ids to their objects

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ process.argv.forEach(function(val, index, array) {
 
 // If the file dosen't exists creates an empty JSON
 if (!fs.existsSync(dataFile)) {
-	fs.writeFile(dataFile, "{}", function() {
-	});
+		fs.writeFileSync(dataFile, "{}");
 }
 
 // File contents to memory
@@ -80,7 +79,7 @@ var Cantrip = {
     				route = temp;
     			} else {
     				//If it's still undefined, return
-    				this.response.send("Requested node doesn't exist");
+    				this.response.status(404).send({"error": "Requested node doesn't exists."});
     				return;
     			}
     		}
@@ -107,7 +106,7 @@ var Cantrip = {
     				route = temp;
     			} else {
     				//If it's still undefined, return
-    				this.response.send("Requested node doesn't exist");
+    				response.status(404).send({"error": "Requested node doesn't exists."});
     				return;
     			}
     		}
@@ -146,14 +145,22 @@ var Cantrip = {
 			//var parent = self.getParentNode(data);
 			//If it's an array, post the new entry to that array
 			if (_.isArray(target)) {
-				var newEntry = _.extend({
-					_id : md5(JSON.stringify(request.body) + (new Date()).getTime() + Math.random() )
-				}, request.body);
-				target.push(newEntry);
-				response.send(target);
+				//Add ids to all objects within arrays in the sent object
+				self.addIdsToModels(request.body);
+				//If the posted body is an object itself, add an id to it
+				if (_.isObject(request.body) && !_.isArray(request.body)) {
+					//Extend the whole object with an _id property, but only if it doesn't already have one
+					request.body = _.extend({
+						_id : md5(JSON.stringify(request.body) + (new Date()).getTime() + Math.random() )
+					}, request.body);
+				}
+				//Push it to the target array
+				target.push(request.body);
+
+				response.send(request.body);
 				self.saveData();
 			} else {
-				response.send("Whoops, cant post to an object. Use PUT instead");
+				response.status(400).send({"error": "Can't POST to an object. Use PUT instead."});
 			}
 		});
 	},
@@ -165,11 +172,12 @@ var Cantrip = {
 		this.getData(function(data) {
 			var target = self.getTargetNode(data);
 			if (_.isObject(target)) {
+				self.addIdsToModels(request.body);
 				target = _.extend(target, request.body);
 				response.send(target);
 				self.saveData();
 			} else {
-				response.send("Whoops, cant put a collection.");
+				response.status(400).send({"error": "Can't PUT a collection."});
 			}
 		});
 	},
@@ -179,25 +187,49 @@ var Cantrip = {
 		this.path = this.getPath();
 		var self = this;
 		this.getData(function(data) {
+			//Get the parent node so we can unset the target
 			var parent = self.getParentNode(data);
+			//Last identifier in the path
 			var index = _.last(self.path);
+			//If it's an object (not an array), then we just unset the key with the keyword delete
 			if (_.isObject(parent) && !_.isArray(parent)) {
-				delete parent[index];
+				//We're not letting users delete the _id
+				if (index === "_id") {
+					response.status(400).send({"error": "You can't delete the id of an object."});
+				} else {
+					delete parent[index];
+				}
+			//If it's an array, we must remove it by id with the splice method	
 			} else if (_.isArray(parent)) {
+				//If the index is a number, index will be the actual index in the array
 				if (_.isNumber(Number(index)) && !_.isNaN(Number(index))) {
 					parent.splice(index, 1);
+				//If it's a hash (string), we find the target object, get it's index and remove it from the array that way
 				} else {
-					console.log(index);
 					var obj = _.find(parent, function(obj) {
 	    				return obj._id === index;
 	    			});
-	    			console.log(_.indexOf(parent, obj));
 	    			parent.splice(_.indexOf(parent, obj), 1);
 				}
 			}
 			response.send(parent);
 			self.saveData();
 		});
+	},
+	addIdsToModels: function(obj) {
+		for (var key in obj) {
+			if ( _.isArray( obj[key] ) ) {
+				for (var i = 0; i < obj[key].length; i++) {
+					if ( _.isObject(obj[key][i]) && !_.isArray(obj[key][i]) ) {
+						obj[key][i] = _.extend({
+							_id : md5(JSON.stringify(obj[key][i]) + (new Date()).getTime() + Math.random() )
+						}, obj[key][i]);
+					}
+				}
+			} else if ( _.isObject( obj[key] ) ) {
+				this.addIdsToModels( obj[key] );
+			}
+		}
 	}
 }
 
