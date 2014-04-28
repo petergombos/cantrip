1da78cda5c0bc2a7162768613bfe0a7d0c6345cb
Márton Borlay <marton.borlay@kriek.hu>
2014-04-28 23:16:34 +0200

    PUT and POST tests added

--- tests/cantripSpec.js
+++ tests/cantripSpec.js
@@ -1,59 +1,109 @@
 var request = require("request");
-var serverUrl = "http://localhost:3000/"
 
 describe("Cantrip is a database-less REST API library saving to a JSON file", function() {
 
 	var Cantrip = require("../index.js");
+	Cantrip.options.port = 3001;
+	var serverUrl = "http://localhost:3001/";
 
 	it("should initialize", function() {
 		expect(Cantrip).toBeDefined();
 	});
 
+	Cantrip.options.file = "tests/test" + Math.floor(Math.random() * 10000000000) + ".json";
+
 	Cantrip.start();
 
-	describe("The REST API", function() {
-		//Overwrite the JSON
-		beforeEach(function() {
-			Cantrip.data = {
-				rootObject: {
-					memberObject: {
-						nestedParameter: 4,
-						nestedParameter2: true,
-						nestedParameter3: {
-							foo: "bar"
-						}
-					},
-					memberArray: [
-						{
-							name: "Object Member",
-							foo: 3
-						},
-						[1,2,3],
-						true
-					],
-					memberParameter: "sample String"
-				},
-				rootArray: [
-					{
-						name: "Model Member",
-					},
-					[7,8,9],
-					false
-				],
-				rootParameter: true
-			}
+
+	describe("Basic REST API functions", function() {
+
+		describe("PUT", function() {
+
+			it("should allow you to put the root object", function(done) {
+				request({
+						method: "PUT",
+						url: serverUrl,
+						json: {foo: "bar"} 
+					}, function(error, response, body) {
+					expect(body).toEqual({foo: "bar"});
+					done();
+				});
+			});
+
+			it("should allow you to modify previous values", function(done) {
+				request({
+						method: "PUT",
+						url: serverUrl,
+						json: {foo: {string: "some string", collection : []}}
+					}, function(error, response, body) {
+					expect(body.foo).toEqual({string: "some string", collection : []});
+					done();
+				});
+			});
+
+			it("should allow you to put children", function(done) {
+				request({
+						method: "PUT",
+						url: serverUrl + "foo",
+						json: {string: "other string"}
+					}, function(error, response, body) {
+					expect(body).toEqual({string: "other string", collection : []});
+					done();
+				});
+			});
+
+			it("should have all modifications up until now saved", function(done) {
+				request({
+						method: "GET",
+						url: serverUrl,
+						json: true
+					}, function(error, response, body) {
+					expect(body).toEqual({foo: {string: "other string", collection : []}});
+					done();
+				});
+			})
+
 		});
 
-		describe("GET", function() {
+		describe("POST", function() {
 
-			it("should get you the root object", function() {
-				request(serverUrl, function(error, response, body) {
-					console.log(body);
+			it("should throw an error when posting to something other than a collection", function(done) {
+				request({
+						method: "POST",
+						url: serverUrl,
+						json: {"foo": "bar"}
+					}, function(error, response, body) {
+					expect(body.error).toBeDefined();
+					done();
+				});
+			});
+
+			it("should allow you to post to a collection and generate you an id", function(done) {
+				request({
+						method: "POST",
+						url: serverUrl + "foo/collection",
+						json: {"foo": "bar"}
+					}, function(error, response, body) {
+					expect(body.foo).toEqual("bar");
+					expect(body._id).toBeDefined();
+					done();
+				});
+			});
+
+			it("should allow you to post to a collection with a specific id and not overwrite it", function(done) {
+				request({
+						method: "POST",
+						url: serverUrl + "foo/collection",
+						json: {"foo": "bar", "_id" : "imanid"}
+					}, function(error, response, body) {
+					expect(body._id).toEqual("imanid");
 					done();
 				});
 			});
 
 		});
+
+		
 
 	});
 
