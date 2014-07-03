30eec28644aa8d4f0933090ddec4845ec88181e4
Márton Borlay <marton.borlay@kriek.hu>
2014-07-03 17:22:46 +0200

    Added default persistence from repository

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var md5 = require('MD5');
 var cors = require('cors');
 var bodyParser = require('body-parser')
+var jsonPersistence = require('cantrip-persistence-json');
 
 //Set up express
 var app = express();
@@ -26,103 +27,17 @@ var Cantrip = {
 		port: process.env.PORT || 3000,
 		saveEvery: 1,
 		namespace: "data"
+		persistence: jsonPersistence
 	},
 	/**
-	 * The app's data should be accessed through this object's methods
+	 * The app's data should be accessed through this object's methods. Provided by the persistence layer
 	 * @type {Object}
 	 */
 	dataStore: {
-		get: function(path, callback) {
-			path = _.filter(path.split("/"), function(string) {
-				return string !== "";
-			});
-			//Get the root element based on several factors: whether we have _contents in the JSON, did we try to access something inside a _meta parameter
-			//By default the root is the whole JSON
-			var node = Cantrip.data;
-			//If we're trying to access a meta object
-			if (path.length > 0 && path[0][0] === "_" && path[0] !== "_meta") {
-				//Set the root object to that meta object, or throw an error if it doesn't exist
-				if (Cantrip.data[path[0]] !== undefined) {
-					node = Cantrip.data[path[0]];
-					var metaObject = path.shift();
-				} else {
-					callback({
-						status: 404,
-						error: "Requested meta object doesn't exist."
-					}, null);
-					return;
-				}
-				//If the first member of the url is "_meta", set the node root to Cantrip.data
-			} else if (path[0] === "_meta") {
-				node = Cantrip.data;
-				var metaObject = path.shift();
-				//If the first member of the url is not a meta object key, then check if we have _contents
-			} else if (Cantrip.data._contents) {
-				node = Cantrip.data._contents;
-			}
-
-			//Loop through the data by the given paths
-			for (var i = 0; i < path.length; i++) {
-				var temp = node[path[i]];
-				//If we found the given key, assign the node object to its value
-				if (temp !== undefined) {
-					node = node[path[i]];
-					//If the given key doesn't exist, try the _id
-				} else {
-					temp = _.find(node, function(obj) {
-						return obj._id === path[i];
-					});
-					//If it's not undefined, then assign it as the value
-					if (temp !== undefined) {
-						node = temp;
-					} else {
-						callback({
-							status: 404,
-							error: "Requested node doesn't exist."
-						}, null);
-						return;
-					}
-				}
-			}
-
-			callback(null, node);
-		},
-		set: function(path, data, callback) {
-			this.get(path, function(err, target) {
-				if (_.isArray(target)) {
-					target.push(data);
-				} else if (_.isObject(target)) {
-					target = _.extend(target, data);
-				} else {
-					target = data;
-				}
-				callback();
-			});
-		},
-		delete: function(path, callback) {
-			var index = _.last(path.split("/"));
-			this.parent(path, function(err, parent) {
-				if (_.isArray(parent)) {
-					if (_.isNumber(Number(index)) && !_.isNaN(Number(index))) {
-						parent.splice(index, 1);
-						//If it's a hash (string), we find the target object, get it's index and remove it from the array that way
-					} else {
-						var obj = _.find(parent, function(obj) {
-							return obj._id === index;
-						});
-						parent.splice(_.indexOf(parent, obj), 1);
-					}
-				} else if (_.isObject(parent)) {
-					delete parent[index];
-				}
-				callback();
-			});
-		},
-		parent: function(path, callback) {
-			this.get(path.split("/").slice(0, -1).join("/"), function(err, parent) {
-				callback(err, parent);
-			});
-		}
+		get: function(){},
+		set: function(){},
+		delete: function(){},
+		parent: function(){}
 	},
 	/**
 	 * Starts the server. Sets up the data in memory, creates a file if necessary
@@ -138,6 +53,8 @@ var Cantrip = {
 			}
 		});
 
+		//Add the loaded persistence layer's methods to the Cantrip object
+		this = _.extend(this, this.options.persistence);
 		//Set up our persistence layer (JSON file or mongodb)
 		this.setupPersistence();
 
@@ -200,19 +117,11 @@ var Cantrip = {
 	/**
 	 * Sets up the persistence (file, database etc.) required for saving data.
 	 * Also sets up the Cantrip.data attribute which holds functions for accessing the data directly
+	 * Provided by the persistence layer
 	 * By default this means reading a file and loading its contents as JSON into memory
 	 */
 	setupPersistence: function() {
-		//Set up memory by reading the contents of the file
-		if (!fs.existsSync("data/" + this.options.namespace + ".json")) {
-			fs.writeFileSync("data/" + this.options.namespace + ".json", "{}");
-		}
-
-		this.data = fs.readFileSync("data/" + this.options.namespace + ".json", {
-			encoding: 'utf-8'
-		});
-
-		this.data = JSON.parse(this.data);
+		
 	},
 
 
