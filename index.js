f7ab968ea59035c18a22c77bf6102b42aa3dc9ed
Márton Borlay <marton.borlay@kriek.hu>
2015-03-10 12:11:23 +0100

    Added json dataStore to the repo

--- index.js
+++ index.js
@@ -1,166 +1,146 @@
 var _ = require("lodash");
 var fs = require('fs');
 var md5 = require('MD5');
-var jsonPersistence = require('cantrip-persistence-json');
+var dataStore = require('./lib/dataStore');
 
-module.exports = function cantrip(options, callback) {
+module.exports = function cantrip(options) {
 	
-	if (typeof options === "function") {
-		callback = options;
-		options = {};
-	}
-	options = _.extend({
-		persistence: jsonPersistence(options)
-	}, options);
+	options = options || {};
 
-	options.persistence(function(err, dataStore) {
+	var dataStore = dataStore(options);
 
-		if (err) {
-			return callback && typeof callback === 'function' && callback(err);
+	/**
+	 * This method handles the request
+	 */
+	var handle = function(req, res, next) {
+		targetNode(req, res, next, function() {
+			if (req.method === "GET") {
+				get(req, res, next);
+			} else if (req.method === "POST") {
+				post(req, res, next);
+			} else if (req.method === "PUT") {
+				put(req, res, next);
+			} else if (req.method === "PATCH") {
+				put(req, res, next, true);
+			} else if (req.method === "DELETE") {
+				del(req, res, next);
+			}
+		});
+	};
+
+	/**
+	 * Gets the target node from the data. Throws an error if it doesn't exist
+	 */
+	var targetNode = function(req, res, next, callback) {
+		dataStore.get(req.path, function(error, data) {
+			if (error) {
+				return next(error);
+			}
+			req.targetNode = data;
+			callback();
+		});
+	};
+	var get = function(req, res, next) {
+		if (_.isObject(req.targetNode) || _.isArray(req.targetNode)) {
+			res.body = _.cloneDeep(req.targetNode);
+			next();
+		} else {
+			res.body = {
+				value: req.targetNode
+			};
+			next();
 		}
-
-		/**
-		 * This method handles the request
-		 */
-		var handle = function(req, res, next) {
-			targetNode(req, res, next, function() {
-				if (req.method === "GET") {
-					get(req, res, next);
-				} else if (req.method === "POST") {
-					post(req, res, next);
-				} else if (req.method === "PUT") {
-					put(req, res, next);
-				} else if (req.method === "PATCH") {
-					put(req, res, next, true);
-				} else if (req.method === "DELETE") {
-					del(req, res, next);
-				}
-			});
-		};
-
-		/**
-		 * Gets the target node from the data. Throws an error if it doesn't exist
-		 */
-		var targetNode = function(req, res, next, callback) {
-			dataStore.get(req.path, function(error, data) {
-				if (error) {
-					return next(error);
-				}
-				req.targetNode = data;
-				callback();
-			});
-		};
-		var get = function(req, res, next) {
-			if (_.isObject(req.targetNode) || _.isArray(req.targetNode)) {
-				res.body = _.cloneDeep(req.targetNode);
-				next();
-			} else {
-				res.body = {
-					value: req.targetNode
-				};
-				next();
+	};
+	var post = function(req, res, next) {
+		//If it's an array, post the new entry to that array
+		if (_.isArray(req.targetNode)) {
+			//Add ids to all objects within arrays in the sent object
+			addMetadataToModels(req.body);
+			//If the posted body is an object itself, add an id to it
+			if (_.isObject(req.body) && !_.isArray(req.body)) {
+				//Extend the whole object with an _id property, but only if it doesn't already have one
+				req.body = _.extend({
+					_id: md5(JSON.stringify(req.body) + (new Date()).getTime() + Math.random()),
+					_createdDate: (new Date()).getTime(),
+					_modifiedDate: (new Date()).getTime()
+				}, req.body);
 			}
-		};
-		var post = function(req, res, next) {
-			//If it's an array, post the new entry to that array
-			if (_.isArray(req.targetNode)) {
-				//Add ids to all objects within arrays in the sent object
-				addMetadataToModels(req.body);
-				//If the posted body is an object itself, add an id to it
-				if (_.isObject(req.body) && !_.isArray(req.body)) {
-					//Extend the whole object with an _id property, but only if it doesn't already have one
-					req.body = _.extend({
-						_id: md5(JSON.stringify(req.body) + (new Date()).getTime() + Math.random()),
-						_createdDate: (new Date()).getTime(),
-						_modifiedDate: (new Date()).getTime()
-					}, req.body);
-				}
-				//Check if the given ID already exists in the collection
-				for (var i = 0; i < req.targetNode.length; i++) {
-					if (req.targetNode[i]._id === req.body._id) {
-						return next({
-							status: 400,
-							error: "An object with the same _id already exists in this collection."
-						});
-					}
-				}
-				//Push it to the target array
-				dataStore.set(req.path, req.body, false, function() {
-					//Set response
-					res.body = _.cloneDeep(req.body);
-					next();
-
-				});
-			} else {
-				return next({
-					status: 400,
-					error: "Can't POST to a resource. Use PUT instead."
-				});
-			}
-		};
-		var put = function(req, res, next, patch) {
-			if (_.isObject(req.targetNode) && !_.isArray(req.targetNode)) {
-				addMetadataToModels(req.body);
-				//If the target had previously had a _modifiedDate property, set it to the current time
-				if (req.targetNode._modifiedDate) req.body._modifiedDate = (new Date()).getTime();
-				var save = function() {
-					dataStore.set(req.path, req.body, patch, function(err, status) {
-						//Set response
-						res.body = {
-							"success": true
-						};
-						return next();
+			//Check if the given ID already exists in the collection
+			for (var i = 0; i < req.targetNode.length; i++) {
+				if (req.targetNode[i]._id === req.body._id) {
+					return next({
+						status: 400,
+						error: "An object with the same _id already exists in this collection."
 					});
-				};
-				//If it's an element inside a collection, make sure the overwritten _id is not present in the collection
-				if (req.body._id && req.targetNode._id && req.body._id !== req.targetNode._id) {
-					dataStore.parent(req.path, function(err, parent) {
-						req.parentNode = parent;
-						for (var i = 0; i < parent.length; i++) {
-							if (parent[i]._id === req.body._id) {
-								return next({
-									status: 400,
-									error: "An object with the same _id already exists in this collection."
-								});
-							}
+				}
+			}
+			//Push it to the target array
+			dataStore.set(req.path, req.body, false, function() {
+				//Set response
+				res.body = _.cloneDeep(req.body);
+				next();
+
+			});
+		} else {
+			return next({
+				status: 400,
+				error: "Can't POST to a resource. Use PUT instead."
+			});
+		}
+	};
+	var put = function(req, res, next, patch) {
+		if (_.isObject(req.targetNode) && !_.isArray(req.targetNode)) {
+			addMetadataToModels(req.body);
+			//If the target had previously had a _modifiedDate property, set it to the current time
+			if (req.targetNode._modifiedDate) req.body._modifiedDate = (new Date()).getTime();
+			var save = function() {
+				dataStore.set(req.path, req.body, patch, function(err, status) {
+					//Set response
+					res.body = {
+						"success": true
+					};
+					return next();
+				});
+			};
+			//If it's an element inside a collection, make sure the overwritten _id is not present in the collection
+			if (req.body._id && req.targetNode._id && req.body._id !== req.targetNode._id) {
+				dataStore.parent(req.path, function(err, parent) {
+					req.parentNode = parent;
+					for (var i = 0; i < parent.length; i++) {
+						if (parent[i]._id === req.body._id) {
+							return next({
+								status: 400,
+								error: "An object with the same _id already exists in this collection."
+							});
 						}
-						//I there was no such problem
-						save();
+					}
+					//I there was no such problem
+					save();
+				});
+			} else {
+				save();
+			}
+		} else {
+			return next({
+				status: 400,
+				error: "Can't PUT a collection. Use POST instead."
+			});
+		}
+	};
+	var del = function(req, res, next) {
+		//Get the parent node so we can unset the target
+		dataStore.parent(req.path, function(err, parent) {
+			//Last identifier in the path
+			var index = _.last(req.path.split("/"));
+			//If it's an object (not an array), then we just unset the key with the keyword delete
+			if (_.isObject(parent) && !_.isArray(parent)) {
+				//We're not letting users delete the _id
+				if ((index + "")[0] === "_") {
+					return next({
+						status: 400,
+						error: "You can't delete an object's metadata."
 					});
 				} else {
-					save();
-				}
-			} else {
-				return next({
-					status: 400,
-					error: "Can't PUT a collection. Use POST instead."
-				});
-			}
-		};
-		var del = function(req, res, next) {
-			//Get the parent node so we can unset the target
-			dataStore.parent(req.path, function(err, parent) {
-				//Last identifier in the path
-				var index = _.last(req.path.split("/"));
-				//If it's an object (not an array), then we just unset the key with the keyword delete
-				if (_.isObject(parent) && !_.isArray(parent)) {
-					//We're not letting users delete the _id
-					if ((index + "")[0] === "_") {
-						return next({
-							status: 400,
-							error: "You can't delete an object's metadata."
-						});
-					} else {
-						dataStore.delete(req.path, function() {
-							//Set response
-							res.body = {
-								"success": true
-							};
-							return next();
-						});
-					}
-					//If it's an array, we must remove it by id with the splice method	
-				} else if (_.isArray(parent)) {
 					dataStore.delete(req.path, function() {
 						//Set response
 						res.body = {
@@ -169,41 +149,50 @@ module.exports = function cantrip(options, callback) {
 						return next();
 					});
 				}
-
-			});
-		};
-		//Recursively add _ids to all objects within an array (but not arrays) within the specified object.
-		var addMetadataToModels = function(obj) {
-			//Loop through the objects keys
-			for (var key in obj) {
-				//If the value of the key is an array (means it's a collection), go through all of its contents
-				if (_.isArray(obj[key])) {
-					for (var i = 0; i < obj[key].length; i++) {
-						//Assign an id to all objects
-						if (_.isObject(obj[key][i]) && !_.isArray(obj[key][i])) {
-							obj[key][i] = _.extend({
-								_id: md5(JSON.stringify(obj[key][i]) + (new Date()).getTime() + Math.random()),
-								_createdDate: (new Date()).getTime(),
-								_modifiedDate: (new Date()).getTime()
-							}, obj[key][i]);
-							//Modify the _modifiedDate metadata property
-							obj[key][i]._modifiedDate = (new Date()).getTime();
-						}
-					}
-					//If it's an object, call the recursive method with that object
-				} else if (_.isObject(obj[key])) {
-					addMetadataToModels(obj[key]);
-				}
+				//If it's an array, we must remove it by id with the splice method	
+			} else if (_.isArray(parent)) {
+				dataStore.delete(req.path, function() {
+					//Set response
+					res.body = {
+						"success": true
+					};
+					return next();
+				});
 			}
-		};
 
-		handle.get = dataStore.get;
-		handle.post = dataStore.post;
-		handle.delete = dataStore.delete;
-		handle.put = dataStore.put;
-		
-		callback && callback(null, handle);
-	});
+		});
+	};
+	//Recursively add _ids to all objects within an array (but not arrays) within the specified object.
+	var addMetadataToModels = function(obj) {
+		//Loop through the objects keys
+		for (var key in obj) {
+			//If the value of the key is an array (means it's a collection), go through all of its contents
+			if (_.isArray(obj[key])) {
+				for (var i = 0; i < obj[key].length; i++) {
+					//Assign an id to all objects
+					if (_.isObject(obj[key][i]) && !_.isArray(obj[key][i])) {
+						obj[key][i] = _.extend({
+							_id: md5(JSON.stringify(obj[key][i]) + (new Date()).getTime() + Math.random()),
+							_createdDate: (new Date()).getTime(),
+							_modifiedDate: (new Date()).getTime()
+						}, obj[key][i]);
+						//Modify the _modifiedDate metadata property
+						obj[key][i]._modifiedDate = (new Date()).getTime();
+					}
+				}
+				//If it's an object, call the recursive method with that object
+			} else if (_.isObject(obj[key])) {
+				addMetadataToModels(obj[key]);
+			}
+		}
+	};
+
+	handle.get = dataStore.get;
+	handle.post = dataStore.post;
+	handle.delete = dataStore.delete;
+	handle.put = dataStore.put;
+	
+	return handle;
 	
 }
 
--- server.js
+++ server.js
@@ -2,35 +2,34 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 
-require("./index.js")(function(err, cantrip) {
+var cantrip = require("./index.js")();
 
-	var app = express();
-	app.use(bodyParser.json());
-	app.use(function(err, req, res, next) {
-		return next({
-			status: 400,
-			error: "Invalid JSON supplied in request body."
-		});
+var app = express();
+app.use(bodyParser.json());
+app.use(function(err, req, res, next) {
+	return next({
+		status: 400,
+		error: "Invalid JSON supplied in request body."
 	});
-
-	app.use(bodyParser.urlencoded());
-	app.use(cors());
-
-	app.use(cantrip);
-
-	app.use(function(req, res, next) {
-		res.send(res.body);
-	});
-
-	app.use(function(err, req, res, next) {
-		if (err.status) res.status(err.status);
-		res.send({
-			error: err.error
-		});
-	});
-
-	app.listen(3000);
-
 });
+
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+app.use(cantrip);
+
+app.use(function(req, res, next) {
+	res.send(res.body);
+});
+
+app.use(function(err, req, res, next) {
+	if (err.status) res.status(err.status);
+	res.send({
+		error: err.error
+	});
+});
+
+app.listen(3000);
+
 
 
